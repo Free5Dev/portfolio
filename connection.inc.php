19417bb200e02c026c3652acd310fa18c9df088e
Thierno SOUMAH <thsoum>
2019-07-24 10:56:07 +0200

    retire lien connexion One and One

--- connection.inc.php
+++ connection.inc.php
@@ -1,7 +1,7 @@
 <?php 
 	try
 	{
-		$bdd=new PDO('mysql:host=db693303573.db.1and1.com;port=3306;dbname=db693303573','dbo693303573','SaidSoumah.91');
+		$bdd=new PDO('mysql:host=localhost;dbname=portfolio','root','');
 	}
 	catch(Exception $e)
 	{
